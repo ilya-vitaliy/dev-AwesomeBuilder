53b8dd947bea3af2527a0ff916a7ed93a184a618
Ilya Poleshko <ilya.poleshko96@gmail.com>
2025-09-09 01:00:01 +0300

    feat:Added position animation and start of Robin pet project.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
-import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
+const getLoremImage = () => {
+  const images = document.querySelectorAll('img');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+  images.forEach((item) => {
+    if (!item.getAttribute('src')) {
+      fetch('https://picsum.photos/640/360')
+        .then((response) => response.blob())
+        .then((blob) => (item.src = URL.createObjectURL(blob)))
+        .catch((error) => (item.src = 'assets/img/cat-scottish.webp'));
+    }
+  });
+};
 
-setupCounter(document.querySelector('#counter'));
+const burgerBtn = document.querySelector('#burger');
+const mobileContainer = document.querySelector('#mobile-container');
+
+burgerBtn.addEventListener('click', () => {
+  mobileContainer.classList.toggle('hidden');
+  burgerBtn.classList.toggle('after:bg-purple');
+  burgerBtn.classList.toggle('before:bg-purple');
+});
+
+document.querySelectorAll('img').length > 0 ? getLoremImage() : null;

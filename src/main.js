4e0f0897654797abc1d31643ab7b19a4a98d8029
Ilya Poleshko <ilya.poleshko96@gmail.com>
2025-09-24 15:01:50 +0300

    feat:partners done

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const getLoremImage = () => {
       fetch('https://picsum.photos/640/360')
         .then((response) => response.blob())
         .then((blob) => (item.src = URL.createObjectURL(blob)))
-        .catch((error) => (item.src = 'assets/img/cat-scottish.webp'));
+        .catch(() => (item.src = 'assets/img/cat-scottish.webp'));
     }
   });
 };
@@ -15,11 +15,45 @@ const burgerBtn = document.querySelector('#burger');
 const mobileContainer = document.querySelector('#mobile-container');
 const body = document.querySelector('body');
 
-burgerBtn.addEventListener('click', () => {
-  mobileContainer.classList.toggle('hidden');
-  burgerBtn.classList.toggle('after:bg-purple');
-  burgerBtn.classList.toggle('before:bg-purple');
-  body.classList.toggle('overflow-hidden');
-});
+if (burgerBtn && mobileContainer) {
+  burgerBtn.addEventListener('click', () => {
+    mobileContainer.classList.toggle('hidden');
+    burgerBtn.classList.toggle('after:bg-purple');
+    burgerBtn.classList.toggle('before:bg-purple');
+    body.classList.toggle('overflow-hidden');
+  });
+}
 
-document.querySelectorAll('img').length > 0 ? getLoremImage() : null;
+let swiper;
+
+function myFunction(Matchmedia_768) {
+  if (swiper) swiper.destroy(true, true); 
+
+  if (Matchmedia_768.matches) {
+    swiper = new Swiper('.swiper', {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      grid: {
+        rows: 3,
+        fill: 'row',
+      },
+      spaceBetween: 20,
+      loop: false,
+    });
+  } else {
+    swiper = new Swiper('.swiper', {
+      slidesPerView: 4,
+      slidesPerGroup: 1,
+      loop: true,
+    });
+  }
+}
+
+const Matchmedia_768 = window.matchMedia('(max-width: 768px)');
+
+myFunction(Matchmedia_768);
+
+Matchmedia_768.addEventListener('change', () => myFunction(Matchmedia_768));
+
+if (document.querySelectorAll('img').length > 0) getLoremImage();
+
